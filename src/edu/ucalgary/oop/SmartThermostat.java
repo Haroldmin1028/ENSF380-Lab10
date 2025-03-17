64762e54d7ec75b7962ca746fda43188453135dd
Hania Aamir <147211103+haniaaaamir@users.noreply.github.com>
2025-03-17 12:35:27 -0600

    Updated SmartThermostat.java

--- src/edu/ucalgary/oop/SmartThermostat.java
+++ src/edu/ucalgary/oop/SmartThermostat.java
@@ -1,6 +1,31 @@
 // TODO: package, any includes, class declaration, constructor
 //       and the methods performAction and update.
 
+package edu.ucalgary.oop
+
+public class SmartThermostat extends SmartDevice<Integer> {
+    public SmartThermostat(String name, int initialTemperature) {
+        this.name = name;
+        this.state = initialTemperature;
+    }
+
+    @Override
+    public void update(String message) {
+        if ("Sleep".equals(message)) {
+            setState(18);
+            System.out.println(name + " is set to 18°C. Adjusting temperature.");
+        } else if ("Vacation".equals(message)) {
+            setState(20);
+            System.out.println(name + " is set to 20°C. Adjusting temperature.");
+        }
+    }
+
+    @Override
+    public void performAction(Integer newTemperature) {
+        setState(newTemperature);
+        System.out.println(name + " is set to " + newTemperature + "°C. Adjusting temperature.");
+    }
+
     public void adjustTemperature(int desiredTemp) {
         int currentTemp = getState();
         int difference = desiredTemp - currentTemp;
