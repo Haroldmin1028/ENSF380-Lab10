7e6079dc97788348aaa0d3133d53cbeff073aad4
Hania Aamir <147211103+haniaaaamir@users.noreply.github.com>
2025-03-17 12:38:16 -0600

    Updated SmartLock.java

--- src/edu/ucalgary/oop/SmartLock.java
+++ src/edu/ucalgary/oop/SmartLock.java
@@ -1,6 +1,47 @@
 // TODO: package, any includes, class declaration, constructor
 //       and the methods performAction and update.
 
+package edu.ucalgary.oop
+
+public class SmartLock extends SmartDevice<Boolean> {
+    public SmartLock(String name) {
+        this.name = name;
+        this.state = true;
+    }
+
+    @Override
+    public void update(String message) {
+        switch (message.toLowerCase()) {
+            case "sleep":
+            case "vacation":
+                setState(true); 
+                System.out.println(name + " is LOCKED. Securing the door.");
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void performAction(String action) {
+        switch (action.toLowerCase()) {
+            case "lock":
+                setState(true);
+                System.out.println(name + " is LOCKED. Securing the door.");
+                break;
+            case "unlock":
+                setState(false);
+                System.out.println(name + " is UNLOCKED. Door is open.");
+                break;
+            case "auto lock":
+                autoLock(10); 
+                break;
+            default:
+                System.out.println("Invalid action for SmartLock.");
+                break;
+        }
+    }
+
     public void autoLock(int delayInSeconds) {
         System.out.println("Auto-lock enabled. Door will lock in " + delayInSeconds + " seconds.");
         new Thread(() -> {
